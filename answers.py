cc4366c9af92aad4e77c352e8d0fa82683aebcb6
Ogun Isik <ogunisik@gmail.com>
2019-12-03 23:01:23 +0100

    initial commit

---
+++ answers.py
@@ -0,0 +1,171 @@
+import math
+from data import problem1, problem3, problem6
+
+
+def answer1():
+    total_fuel = 0
+    for mass in problem1.MODULES:
+        total_fuel += answer1_get_fuel(mass)
+
+    return total_fuel
+
+
+def answer1_get_fuel(mass):
+    return mass // 3 - 2
+
+
+def answer2():
+    total_fuel = 0
+    for mass in problem1.MODULES:
+        total_fuel += answer2_get_fuel(mass)
+
+    return total_fuel
+
+
+def answer2_get_fuel(mass):
+    total_fuel = answer1_get_fuel(mass)
+
+    if total_fuel > 0:
+        total_fuel += answer2_get_fuel(total_fuel)
+        return total_fuel
+
+    return 0
+
+
+def answer3():
+    states = problem3.STATES
+
+    states[1] = 12
+    states[2] = 2
+
+    final_state = answer3_execute(states)
+    return final_state[0]
+
+
+def answer3_execute(intcode):
+    instructions = {1: lambda x, y: x + y, 2: lambda x, y: x * y}
+
+    for idx in range(0, len(intcode), 4):
+        opcode = intcode[idx : idx + 4]
+        op = opcode[0]
+        if op == 99:
+            break
+
+        val1 = intcode[opcode[1]]
+        val2 = intcode[opcode[2]]
+        idx3 = opcode[3]
+
+        result = instructions[op](val1, val2)
+        if len(intcode) == idx3:
+            intcode.insert(result)
+        else:
+            intcode[idx3] = result
+
+    return intcode
+
+
+def answer4():
+
+    for x in range(100):
+        for y in range(100):
+            states = problem3.STATES.copy()
+            states[1] = x
+            states[2] = y
+
+            try:
+                if 19690720 == answer3_execute(states)[0]:
+                    return x, y
+            except:
+                continue
+
+    return 0
+
+
+def answer5():
+    return answer5_closest_intersection(problem6.WIRES)
+
+
+def answer5_closest_intersection(wires):
+    sets = []
+
+    for wire in wires:
+        paths = wire.split(",")
+
+        new_set = set()
+        current_x = 0
+        current_y = 0
+        for path in paths:
+            direction = path[0:1]
+            step = int(path[1:])
+            if direction == "U":
+                for i in range(current_y + 1, current_y + step + 1):
+                    current_y = i
+                    new_set.add((current_x, current_y))
+            elif direction == "D":
+                for i in range(current_y - 1, current_y - (step + 1), -1):
+                    current_y = i
+                    new_set.add((current_x, i))
+            elif direction == "R":
+                for i in range(current_x + 1, current_x + step + 1):
+                    current_x = i
+                    new_set.add((current_x, current_y))
+            elif direction == "L":
+                for i in range(current_x - 1, current_x - (step + 1), -1):
+                    current_x = i
+                    new_set.add((current_x, current_y))
+        sets.append(new_set)
+
+    intersections = sets[0].intersection(sets[1])
+
+    return min(abs(x[0]) + abs(x[1]) for x in intersections)
+
+
+def answer6():
+    return answer6_closest_path(problem6.WIRES)
+
+
+def answer6_closest_path(wires):
+    sets = []
+    lists = []
+
+    for wire in wires:
+        paths = wire.split(",")
+
+        new_set = set()
+        new_list = list()
+        current_x = 0
+        current_y = 0
+        for path in paths:
+            direction = path[0:1]
+            step = int(path[1:])
+            if direction == "U":
+                for i in range(current_y + 1, current_y + step + 1):
+                    current_y = i
+                    new_set.add((current_x, current_y))
+                    new_list.append((current_x, current_y))
+            elif direction == "D":
+                for i in range(current_y - 1, current_y - (step + 1), -1):
+                    current_y = i
+                    new_set.add((current_x, i))
+                    new_list.append((current_x, i))
+            elif direction == "R":
+                for i in range(current_x + 1, current_x + step + 1):
+                    current_x = i
+                    new_set.add((current_x, current_y))
+                    new_list.append((current_x, current_y))
+            elif direction == "L":
+                for i in range(current_x - 1, current_x - (step + 1), -1):
+                    current_x = i
+                    new_set.add((current_x, current_y))
+                    new_list.append((current_x, current_y))
+        sets.append(new_set)
+        lists.append(new_list)
+
+    intersections = sets[0].intersection(sets[1])
+
+    return min(lists[0].index(x) + lists[1].index(x) + 2 for x in intersections)
+
+
+if __name__ == "__main__":
+    answer = answer6()
+    print(answer)
---
+++ answers_test.py
@@ -0,0 +1,84 @@
+import unittest
+import answers
+
+
+class TestAnswers(unittest.TestCase):
+    def test_answer1(self):
+        tests = [(12, 2), (14, 2), (1969, 654), (100756, 33583)]
+
+        for value, expected in tests:
+            with self.subTest(value=value):
+                self.assertEqual(answers.answer1_get_fuel(value), expected)
+
+    def test_answer2(self):
+        tests = [(14, 2), (1969, 966), (100756, 50346)]
+
+        for value, expected in tests:
+            with self.subTest(value=value):
+                self.assertEqual(answers.answer2_get_fuel(value), expected)
+
+    def test_answer3(self):
+        tests = [
+            ([1, 0, 0, 0, 99], [2, 0, 0, 0, 99]),
+            (
+                [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+                [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+            ),
+            ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
+            ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
+            ([1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
+        ]
+
+        for value, expected in tests:
+            with self.subTest(value=value):
+                self.assertEqual(answers.answer3_execute(value), expected)
+
+    def test_answer5(self):
+        tests = [
+            (["R8,U5,L5,D3", "U7,R6,D4,L4"], 6),
+            (
+                [
+                    "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                    "U62,R66,U55,R34,D71,R55,D58,R83",
+                ],
+                159,
+            ),
+            (
+                [
+                    "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                    "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                ],
+                135,
+            ),
+        ]
+
+        for value, expected in tests:
+            with self.subTest(value=value):
+                self.assertEqual(answers.answer5_closest_intersection(value), expected)
+
+    def test_answer6(self):
+        tests = [
+            (["R8,U5,L5,D3", "U7,R6,D4,L4"], 30),
+            (
+                [
+                    "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                    "U62,R66,U55,R34,D71,R55,D58,R83",
+                ],
+                610,
+            ),
+            (
+                [
+                    "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                    "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+                ],
+                410,
+            ),
+        ]
+
+        for value, expected in tests:
+            with self.subTest(value=value):
+                self.assertEqual(answers.answer6_closest_path(value), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
